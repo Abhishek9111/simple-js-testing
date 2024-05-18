820bfc68d4b14b971c3ee4fd02a28cf29ab67f7f
Abhishek Kapoor <42829419+Abhishek9111@users.noreply.github.com>
2024-05-18 14:10:02 +0530

    first commit

---
+++ src/tests/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test,it } from "@jest/globals";
+import { sum } from "../index";
+
+describe("sum module", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
